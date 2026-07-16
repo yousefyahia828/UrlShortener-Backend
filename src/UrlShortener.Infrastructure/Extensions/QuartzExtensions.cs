414682b82c2654a3803ca1ed0dc3aaf53f8e4bd5
Yousef Zakaria <yahiaz@std.mans.edu.eg>
2026-07-16 11:06:21 +0300

    Switch to hosted database

--- src/UrlShortener.Infrastructure/Extensions/QuartzExtensions.cs
+++ src/UrlShortener.Infrastructure/Extensions/QuartzExtensions.cs
@@ -20,7 +20,7 @@ internal static class QuartzExtensions
                 storeOptions.UsePostgres(config =>
                 {
                     config.ConnectionString = configuration.GetConnectionString("DefaultConnection")!;
-                    config.ConnectionString = configuration.GetConnectionString("Local")!;
+                    //config.ConnectionString = configuration.GetConnectionString("Local")!;
 
                     config.TablePrefix = "quartz.qrtz_";
                 });
