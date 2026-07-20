b1d06b9a0312bdf7b3c5f99cab3f368516a9e17f
Yousef Zakaria <yahiaz@std.mans.edu.eg>
2026-07-20 19:23:23 +0300

    Fix Email

--- src/UrlShortener.Infrastructure/Extensions/QuartzExtensions.cs
+++ src/UrlShortener.Infrastructure/Extensions/QuartzExtensions.cs
@@ -19,8 +19,8 @@ internal static class QuartzExtensions
 
                 storeOptions.UsePostgres(config =>
                 {
-                    //config.ConnectionString = configuration.GetConnectionString("DefaultConnection")!;
-                    config.ConnectionString = configuration.GetConnectionString("Local")!;
+                    config.ConnectionString = configuration.GetConnectionString("DefaultConnection")!;
+                    //config.ConnectionString = configuration.GetConnectionString("Local")!;
 
                     config.TablePrefix = "quartz.qrtz_";
                 });
