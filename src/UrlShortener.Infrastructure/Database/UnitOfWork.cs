e170672d625f1968361e496a85d8be4da30aad46
Yousef Zakaria <yahiaz@std.mans.edu.eg>
2026-07-28 11:13:59 +0300

    Fix Unit of Work

--- src/UrlShortener.Infrastructure/Database/UnitOfWork.cs
+++ src/UrlShortener.Infrastructure/Database/UnitOfWork.cs
@@ -4,8 +4,8 @@ namespace UrlShortener.Infrastructure.Database;
 
 internal sealed class UnitOfWork(ApplicationDbContext dbContext) : IUnitOfWork
 {
-    public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
-        return dbContext.SaveChangesAsync(cancellationToken);
+        return await dbContext.SaveChangesAsync(cancellationToken);
     }
 }
